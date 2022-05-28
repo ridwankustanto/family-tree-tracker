78ab4e7f56944902802685c37132fc2d36bae023
Ridwan Kustanto <ridwankustanto@gmail.com>
2022-05-28 20:24:20 +0700

    ⛏️ update schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,50 +1,48 @@
-CREATE TABLE IF NOT EXISTS family_origins (
+CREATE TABLE IF NOT EXISTS accounts (
   id CHAR(32) PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  alias VARCHAR(1) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL
+  username VARCHAR(64) NOT NULL,
+  password VARCHAR(1) NOT NULL,
+  is_admin BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS members (
+CREATE TABLE IF NOT EXISTS people (
   id CHAR(32) PRIMARY KEY,
   family_id VARCHAR(64) NOT NULL,
-  name VARCHAR(64) NOT NULL,
+  fullname VARCHAR(64) NOT NULL,
   nickname VARCHAR(16) NOT NULL,
   place_of_birth VARCHAR(64) NOT NULL,
   date_of_birth VARCHAR(64) NOT NULL,
   origin_address VARCHAR(255) NOT NULL,
   current_address VARCHAR(255) NOT NULL,
+  country VARCHAR(32) NOT NULL,
+  state VARCHAR(32) NOT NULL,
+  city VARCHAR(32) NOT NULL,
+  disctrict VARCHAR(32) NOT NULL,
+  subdisctrict VARCHAR(32) NOT NULL,
   mobile_phone VARCHAR(16) NOT NULL,
   phone VARCHAR(16) NOT NULL,
   religion VARCHAR(16) NOT NULL,
-  photo VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS member_has_spouse (
-  id CHAR(32) PRIMARY KEY,
-  member_id CHAR(32) NOT NULL,
-  name VARCHAR(64) NOT NULL,
-  nickname VARCHAR(16) NOT NULL,
-  place_of_birth VARCHAR(64) NOT NULL,
-  date_of_birth VARCHAR(64) NOT NULL,
-  origin_address VARCHAR(255) NOT NULL,
-  photo VARCHAR(255) NOT NULL,
-  CONSTRAINT FK_SpouseMember FOREIGN KEY (member_id)
-  REFERENCES members(member_id)
-);
-
-CREATE TABLE IF NOT EXISTS member_has_childs (
-  id CHAR(32) PRIMARY KEY,
-  member_id CHAR(32) NOT NULL,
-  family_id VARCHAR(64) NOT NULL,
-  name VARCHAR(64) NOT NULL,
-  nickname VARCHAR(16) NOT NULL,
-  place_of_birth VARCHAR(64) NOT NULL,
-  date_of_birth VARCHAR(64) NOT NULL,
-  origin_address VARCHAR(255) NOT NULL,
   gender VARCHAR(1) NOT NULL,
   photo VARCHAR(255) NOT NULL,
-  CONSTRAINT FK_ChildMember FOREIGN KEY (member_id)
-  REFERENCES members(member_id)
+  is_origin BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS people_has_relations (
+  id CHAR(32) PRIMARY KEY,
+  people_id CHAR(32) NOT NULL,
+  with_people_id CHAR(32) NOT NULL,
+  relation_type VARCHAR(8) NOT NULL,
+  CONSTRAINT FK_RelationPeople FOREIGN KEY (people_id)
+  REFERENCES people(id),
+  CONSTRAINT FK_RelationWithPeople FOREIGN KEY (with_people_id)
+  REFERENCES people(id)
+);
+
+CREATE TABLE IF NOT EXISTS people_has_family (
+  id CHAR(32) PRIMARY KEY,
+  family_name VARCHAR(64) NOT NULL,
+  husband_id CHAR(32) NOT NULL,
+  wife_id CHAR(32),
+  CONSTRAINT FK_FamilyPeople FOREIGN KEY (husband_id)
+  REFERENCES people(id)
 );
